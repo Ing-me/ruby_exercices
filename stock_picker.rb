7be1c2258283f5551ffd9e0b00d93af6fd60f6c5
Meide Djhymit <djhymitmeide27@gmail.com>
2024-01-16 12:24:05 -0500

    Complete the stcok_picker exercice

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,4 +1,16 @@
 def stock_picker(arr)
+       
+    # Check the length of the array
+    if (arr.length < 2)
+        puts ("The array should contain at least 2 elements")
+    end
+
+    arr_range = ((0...arr.length).to_a)
+    permuted_array = arr_range.repeated_permutation(2)
+    .select { |start, finish| finish > start}
+    .max_by { |start, finish|  arr[finish] - arr[start]}
+
+   
 
 end
 
